4d84ad4bcbca464a1f6eceefecf13928dc4ddf58
AIsaac10 <isaacmoreira12332@gmail.com>
2026-07-30 18:55:38 -0300

    feat: criar modelo Topic com relacionamentos e campos preenchíveis

---
+++ backend/app/Models/Topic.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Topic extends Model
+{
+    protected $fillable = [
+        'name',
+        'content_id',
+    ];
+
+    public function content(): BelongsTo
+    {
+        return $this->belongsTo(Content::class);
+    }
+
+    public function questions(): BelongsToMany
+    {
+        return $this->belongsToMany(Question::class, 'question_topic');
+    }
+}
